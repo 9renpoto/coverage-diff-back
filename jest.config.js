c5a37061ca0ced2e3b4a83d47bf90d027440d437
Keisuke Kan <9renpoto@gmail.com>
2020-08-13 09:29:02 +0900

    fix: collect coverage from

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,17 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{ts}"],
   coverageDirectory: "coverage",
   coverageReporters: ["json-summary", "text", "text-summary", "lcov"],
   errorOnDeprecated: true,
   globals: {
     "ts-jest": {
-      tsConfig: "tsconfig.json"
-    }
+      tsConfig: "tsconfig.json",
+    },
   },
   moduleFileExtensions: ["ts", "js"],
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*\\.test\\.ts"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  }
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
 };
